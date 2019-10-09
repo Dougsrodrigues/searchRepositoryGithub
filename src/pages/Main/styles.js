f07b63774fea8b0e0b1f3c4fe0423aa27038e901
Douglas <dougsrodrigues@outlook.com>
2019-10-08 22:34:13 -0300

    Finalizando app

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,34 +1,5 @@
 import styled, { keyframes, css } from "styled-components";
 
-export const Container = styled.div`
-  max-width: 700px;
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  padding: 30px;
-  margin: 80px auto;
-
-  h1 {
-    font-size: 20px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-`;
-export const Form = styled.form`
-  margin-top: 30px;
-  display: flex;
-  flex-direction: row;
-
-  input {
-    flex: 1;
-    border: 1px solid #eee;
-    padding: 10px 15px;
-    border-radius: 4px;
-    font-size: 16px;
-  }
-`;
-
 const rotate = keyframes`
   from{
     transform: rotate(0deg);
@@ -83,5 +54,18 @@ export const List = styled.ul`
       color: #7159c1;
       text-decoration: none;
     }
+  }
+`;
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
   }
 `;
